eab7ccbb0ef6b68d091c4411642f6e5fb0425d45
atharvx09 <113002246+atharvx09@users.noreply.github.com>
2025-07-09 01:18:19 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start text-slate-300">
                     <motion.h1  variants={container(0 )} initial="hidden" animate="visible"  className=" mx-8 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl" >Atharv Karole</motion.h1>
-                    <motion.span variants={container(0.5)} initial="hidden" animate="visible" transition={{duration:0.5,delay:0.7}} className="mx-8 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"> SWE@EPAM | MERN Stack Developer</motion.span>
+                    <motion.span variants={container(0.5)} initial="hidden" animate="visible" transition={{duration:0.5,delay:0.7}} className="mx-8 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"> SWE @EPAM | MERN Stack Developer</motion.span>
                     <motion.p variants={container(1)} initial="hidden" animate="visible"  className="my-2 mx-8  max-w-xl font-medium text-xl tracking-tighter"> {HERO_CONTENT}</motion.p>
                     <motion.p variants={container(1)} initial="hidden" animate="visible"  className="my-2 mx-8  max-w-xl font-medium text-xl tracking-tighter"> {HERO_CONTENT2}</motion.p>
                     <motion.p variants={container(1)} initial="hidden" animate="visible"  className="my-2 mx-8  max-w-xl font-medium text-xl tracking-tighter"> {HERO_CONTENT3}</motion.p>
